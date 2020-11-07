6cb9163b2f94a7def8ef3ec625de859dae7b9549
Nikolaev Mikhail <nikolaev.mihail@inbox.ru>
2020-11-07 19:20:33 +0300

    Plot / collect scripts updating

--- collect.py
+++ collect.py
@@ -3,6 +3,26 @@
 import os
 import sys
 
+ref = f'''
+    Usage:
+        {__file__} <out dir> <file name> <result file>
+
+    <out dir>:
+        The path to the directory, contains the output of MPI code.
+
+    <file name>:
+        The name of the files that should be collected among the
+        subdirectories.
+
+    <result file>:
+        The name of the file, that will be used for storing the
+        result of the collecting.
+'''
+
+if (sys.argv[1] == "-h" or sys.argv[1] == "--help"):
+    print(ref)
+    sys.exit()
+
 directory = sys.argv[1]
 filename = sys.argv[2]
 output_file = sys.argv[3]
--- plot.py
+++ plot.py
@@ -6,29 +6,48 @@ import csv
 import sys
 import numpy as np
 
+ref = f'''
+    Usage:
+        {__file__} <data file> <out file> [-d]
+
+    <data file>:
+        The file, contains the data.
+
+    <out file>:
+        The file, that will be used as an output one.
+
+    -d:
+        If specified then it plots the difference between the
+        given and expected data.
+'''
+
+if (sys.argv[1] == "-h" or sys.argv[1] == "--help"):
+    print(ref)
+    sys.exit()
+
 x1 = 0.0
 x2 = 4.0
 y1 = 0.0
 y2 = 3.0
 
-xcount = int(sys.argv[1])
-ycount = int(sys.argv[2])
-out_file = sys.argv[3]
-plot_file_name = sys.argv[4]
+data_file = sys.argv[1]
+plot_file_name = sys.argv[2]
 
-xrange = np.linspace(x1, x2, xcount)
-yrange = np.linspace(y1, y2, ycount)
-xs, ys = np.meshgrid(xrange, yrange)
-
-with open(out_file, "r") as f:
+with open(data_file, "r") as f:
     reader = csv.reader(f, delimiter = ' ')
     zs = np.array([
         [ float(val) for val in item ] for item in reader
     ])
 
+xrange = np.linspace(x1, x2, zs.shape[1])
+yrange = np.linspace(y1, y2, zs.shape[0])
+xs, ys = np.meshgrid(xrange, yrange)
+
 fig = plt.figure()
 hm_axis = fig.add_subplot(1, 1, 1, projection="3d")
 
-real_zs = np.sqrt(xs * ys + 4)
+if (len(sys.argv) > 3 and sys.argv[3] == "-d"):
+    zs = zs - np.sqrt(xs * ys + 4)
+
 hm = hm_axis.plot_surface(xs, ys, zs)
 fig.savefig(plot_file_name)
